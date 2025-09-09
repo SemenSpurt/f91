9cefaa615574b8e8da9b529ac3a593a49bf2a6a1
Semen Selezov <spurtsemen@gmail.com>
2025-09-09 13:50:07 +0300

    fix migration

--- priv/repo/migrations/20250908084400_create_directories.exs
+++ priv/repo/migrations/20250908084400_create_directories.exs
@@ -5,6 +5,7 @@ defmodule F91.Repo.Migrations.CreateDirectories do
     create table(:directories, primary_key: false) do
       add :id, :uuid, primary_key: true
       add :title, :string
+      add :description, :string
 
       timestamps()
     end
